c07c3c19f319ddcf47d21aee150aeb4cbd46ec75
johnnyblame <iwishmynamewasdaniel@gmail.com>
2019-11-27 01:37:25 +0200

    heroku deployment minor changes

--- app/__init__.py
+++ app/__init__.py
@@ -12,29 +12,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-def create_app():
-
-    if not app.debug and not app.testing:
-
-        if app.config['LOG_TO_STDOUT']:
-            stream_handler = logging.StreamHandler()
-            stream_handler.setLevel(logging.INFO)
-            app.logger.addHandler(stream_handler)
-        else:
-            if not os.path.exists('logs'):
-                os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/paymentservice.log',
-                                               maxBytes=10240, backupCount=10)
-            file_handler.setFormatter(logging.Formatter(
-                '%(asctime)s %(levelname)s: %(message)s '
-                '[in %(pathname)s:%(lineno)d]'))
-            file_handler.setLevel(logging.INFO)
-            app.logger.addHandler(file_handler)
-
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('payment-service startup')
-
-    return app
+if __name__ == '__main__':
+    app.run()
 
 
 
