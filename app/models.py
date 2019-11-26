de02e08c23a1bdbadc53ab84e02b816f94744dd2
johnnyblame <iwishmynamewasdaniel@gmail.com>
2019-11-27 00:09:26 +0200

    finished working app

--- app/models.py
+++ app/models.py
@@ -1,49 +1,19 @@
 from app import db
-from config import Config, UPLOAD_FOLDER
-import os
-import json
 
 
 class Payment(db.Model):
     shop_id = db.Column(db.Integer)
     shop_order_id = db.Column(db.Integer)
     amount = db.Column(db.Integer)
-    payway = db.Column(db.String(256), nullable=True)
     currency = db.Column(db.String(10))
     product_info = db.Column(db.String(256), nullable=True)
     sign = db.Column(db.String(256), primary_key=True)
-    lifetime = db.Column(db.Integer, nullable=True)
-    payer_account = db.Column(db.String(128), nullable=True)
-    id = db.Column(db.Integer, nullable=True)
-    url = db.Column(db.String(256), nullable=True)
-
-
-
-    JSON_FILENAME = '.data.json'
+    payway = db.Column(db.String(256), nullable=True)
 
     def __repr__(self):
-        return '<Payment> {}'.format(self.shop_order_id)
+        return '<Payment> {}'.format(self.sign)
 
-    def generate_key(self):
-        hash_i = str('{}:{}:{}:{}:{}').format(self.amount, self.currency, self.shop_id,
-                                              self.shop_order_id, Config.SECRET_KEY).encode('utf-8')
-        key = hash_i.hex()
-        return key
-
-    @classmethod
-    def key_to_path(cls, key):
-        relative_path = os.path.join(key)
-        return relative_path
-
-    @classmethod
-    def get(cls, key):
-        relative_path = cls.key_to_path(key)
-        path = os.path.join(UPLOAD_FOLDER, relative_path)
-        with open(os.path.join(path, key + cls.JSON_FILENAME)) as json_file:
-            infos = json.load(json_file)
-            return cls(**infos)
-
-    def __init__(self, shop_id, shop_order_id, amount, currency, product_info, sign, *args, **kwargs):
+    def __init__(self, shop_id, shop_order_id, amount, currency, product_info, sign, payway, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.shop_id = shop_id
         self.shop_order_id = shop_order_id
@@ -51,14 +21,5 @@ class Payment(db.Model):
         self.currency = currency
         self.product_info = product_info
         self.sign = sign
+        self.payway = payway
 
-    def save(self):
-        self.key = self.generate_key()
-        self.relative_path = self.key_to_path(self.key)
-        path = os.path.join(UPLOAD_FOLDER, self.relative_path)
-        os.makedirs(path)
-        infos = {'shop_id': self.shop_id, 'shop_order_id': self.shop_order_id, 'amount': self.amount,
-                 'currency': self.currency, 'product_info': self.product_info}
-        path = os.path.join(UPLOAD_FOLDER, self.relative_path)
-        with open(os.path.join(path, self.key + self.JSON_FILENAME), 'w') as json_file:
-            json.dump(infos, json_file)
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,13 @@
 import hashlib
 import logging
-import urllib.parse
-import urllib.request
 import requests
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, jsonify
 from app import app, db
 from app.models import Payment
 from config import Config
+import json
 
-logging.basicConfig(filename='error.log', level=logging.DEBUG)
+logging.basicConfig(filename='error.log', level=logging.INFO)
 
 
 @app.route('/')
@@ -18,103 +17,85 @@ def rec():
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        if request.form['currency'] == 'RUB' or request.form['currency'] == 643:
-            if request.method == 'POST':
-                shop_id = 5
-                shop_order_id = 123456
-                payway = 'payeer_rub'
-                amount = request.form['amount']
-                currency = request.form['currency']
-                product_info = request.form['product_info']
-                pure_sign = request.form['pure_sign']
-                pure_sign = '{}:{}:{}:{}{}'.format(amount, currency, shop_id,
-                                                   shop_order_id, Config.SECRET_KEY)
-                sign = hashlib.sha256(pure_sign.encode()).hexdigest()
-                entry = Payment(shop_id, shop_order_id, payway, amount, currency, product_info, sign)
-                db.session.add(entry)
-                db.session.commit()
-                headers = {'Content-Type': 'application/json'}
-                url = 'https://core.piastrix.com/invoice/create'
-                values = {'shop_id': shop_id, 'shop_order_id': shop_order_id, 'payway': payway,
-                          'amount': amount, 'currency': currency, 'description': product_info, 'sign': sign}
-                data = urllib.parse.urlencode(values)
-                req = urllib.request.Request(url, data=data, headers=headers)
-                response = urllib.request.urlopen(req)
-                the_page = response.read()
-                return the_page
-
-        elif request.form['currency'] == 'EUR' or request.form['currency'] == 978:
-            if request.method == 'POST':
-                data = request.form.to_dict()
-                shop_id = request.form['shop_id']
-                shop_order_id = request.form['shop_order_id']
-                amount = request.form['amount']
-                currency = request.form['currency']
-                product_info = request.form['product_info']
-                shop_sign = request.form['shop_sign']
-                pure_sign = request.form['pure_sign']
-                pure_sign = '{}:{}:{}:{}{}'.format(amount, currency, shop_id,
-                                                   shop_order_id, Config.SECRET_KEY)
-                sign = hashlib.sha256(pure_sign.encode()).hexdigest()
-                entry = Payment(shop_id, shop_order_id, amount, currency, product_info, sign)
-                db.session.add(entry)
-                db.session.commit()
-                data['sign'] = sign
-                data['shop_sign'] = sign
-                headers = {'Content-Type': 'application/json'}
-                with open('data.txt', 'w') as file:
-                    file.write(data)
-                url = 'https://pay.piastrix.com/en/pay'
-                # response = requests.post(url, data=data)
-                response = redirect(url)
-                response.headers = data
-                return response
-
-        elif request.form['currency'] == 'USD' or request.form['currency'] == 840:
-            if request.method == 'POST':
-                result = request.form.to_dict()
-                shop_id = 5
-                shop_order_id = 123456
-                shop_amount = request.form['amount']
-                shop_currency = request.form['currency']
-                product_info = request.form['product_info']
-                pure_sign = request.form['pure_sign']
-                pure_sign = '{}:{}:{}:{}{}'.format(shop_amount, shop_currency, shop_id,
-                                                   shop_order_id, Config.SECRET_KEY)
-                sign = hashlib.sha256(pure_sign.encode()).hexdigest()
-                entry = Payment(shop_id, shop_order_id, shop_amount, shop_currency, product_info, sign)
-                db.session.add(entry)
-                db.session.commit()
-                url = 'https://core.piastrix.com/bill/create'
-                params = {'shop_id': shop_id, 'shop_order_id': shop_order_id, 'shop_amount': shop_amount,
-                          'payer_currency': shop_currency, 'shop_currency': shop_currency, 'description': product_info,
-                          'sign': sign}
-                data = urllib.parse.urlencode(params)
-                headers = {'Content-Type': 'application/json'}
-                esreq = requests.Request(url=url, headers=headers, data=values)
-                resp = requests.Session().send(esreq.prepare())
-                # req = urllib.request.Request(url, data=data, headers=headers)
-                # response = urllib.request.urlopen(req)
-                # the_page = response.read()
-                return resp.content
-
     return render_template('index.html', title='PayNow')
 
 
-@app.route('/result', methods=['POST', 'GET'])
-def result():
-    if request.method == 'POST':
-        result = request.form.to_dict()
-        shop_id = request.form['shop_id']
-        shop_order_id = request.form['shop_order_id']
-        amount = request.form['amount']
-        currency = request.form['currency']
-        product_info = request.form['description']
-        pure_sign = request.form['pure_sign']
-        pure_sign = '{}:{}:{}:{}{}'.format(amount, currency, shop_id,
-                                           shop_order_id, Config.SECRET_KEY)
-        sign = hashlib.sha256(pure_sign.encode()).hexdigest()
-        result['sign'] = sign
-        result['pure_sign'] = pure_sign
-        return render_template('result.html', result=result)
+@app.route('/pay_handler', methods=['GET', 'POST'])
+def pay():
+    j = request.get_json()
+    if j['currency'] == 643 or j['currency'] == '643':
+        if request.method == "POST":
+            shop_id = 5
+            shop_order_id = 123456
+            payway = 'payeer_rub'
+            amount = j['amount']
+            currency = j['currency']
+            product_info = j['description']
+            pure_sign = '{}:{}:{}:{}:{}{}'.format(amount, currency, payway, shop_id,
+                                                  shop_order_id, Config.SECRET_KEY)
+            sign = hashlib.sha256(pure_sign.encode('utf8')).hexdigest()
+            entry = Payment(shop_id, shop_order_id, amount, currency, product_info, sign, payway)
+            db.session.add(entry)
+            db.session.commit()
+            url = 'https://core.piastrix.com/invoice/create'
+            params = {'shop_id': shop_id, 'shop_order_id': shop_order_id,
+                      'amount': '{0:.2f}'.format(float(amount)),
+                      'currency': int(currency), 'payway': payway,
+                      'description': product_info, 'sign': sign}
+            headers = {'Content-Type': 'application/json'}
+            data = json.dumps(params)
+            response = requests.post(url, data, headers=headers).json()
+            if response['result'] is True:
+                return jsonify(dict(redirect=response['data']['url']))
+            else:
+                print(response)
+
+    elif j['currency'] == '978' or j['currency'] == 978:
+        if request.method == "POST":
+            shop_id = 5
+            shop_order_id = 101
+            amount = j['amount']
+            currency = j['currency']
+            product_info = j['description']
+            pure_sign = '{}:{}:{}:{}{}'.format(amount, currency, shop_id,
+                                               shop_order_id, Config.SECRET_KEY)
+            sign = hashlib.sha256(pure_sign.encode('utf-8')).hexdigest()
+            entry = Payment(shop_id, shop_order_id, amount, currency, product_info, sign, payway=None)
+            db.session.add(entry)
+            db.session.commit()
+            params = {'shop_id': shop_id, 'shop_order_id': shop_order_id, 'amount': '{0:.2f}'.format(float(amount)),
+                      'currency': int(currency), 'description': product_info, 'sign': sign}
+            url = 'https://pay.piastrix.com/en/pay'
+            headers = {'Content-Type': 'application/json'}
+            data = json.dumps(params)
+            response = requests.post(url, data, headers=headers)
+            return jsonify(dict(redirect=url))
+
+
+    elif j['currency'] == 840 or j['currency'] == '840':
+        if request.method == "POST":
+            shop_id = 5
+            shop_order_id = 123456
+            shop_amount = j['amount']
+            shop_currency = j['currency']
+            product_info = j['description']
+            pure_sign = '{}:{}:{}:{}:{}{}'.format(shop_currency, '{0:.2f}'.format(float(shop_amount)), shop_currency,
+                                                  shop_id, shop_order_id, Config.SECRET_KEY)
+            print(pure_sign)
+            sign = hashlib.sha256(pure_sign.encode('utf-8')).hexdigest()
+            entry = Payment(shop_id, shop_order_id, shop_amount, shop_currency, product_info, sign, payway=None)
+            db.session.add(entry)
+            db.session.commit()
+            url = 'https://core.piastrix.com/bill/create'
+            params = {'shop_id': shop_id, 'shop_order_id': shop_order_id,
+                      'shop_amount': '{0:.2f}'.format(float(shop_amount)),
+                      'payer_currency': int(shop_currency), 'shop_currency': int(shop_currency),
+                      'description': product_info, 'sign': sign}
+            headers = {'Content-Type': 'application/json'}
+            data = json.dumps(params)
+            response = requests.post(url, data, headers=headers).json()
+            if response['result'] is True:
+                return jsonify(dict(redirect=response['data']['url']))
+            else:
+                print(response)
+
